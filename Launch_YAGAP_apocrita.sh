d0c4bbf76cba79a164cba8bde6f012a0698153c1
Richard Heery <richardheery@gmail.com>
2026-04-10 16:25:46 +0100

    Changed to Slurm directives Changed time to 10 days Removed work directory command line parameter and added genome and sample_sheet parameters Changed miniconda to miniforge Commented activate Mikado Conda environment

--- Launch_YAGAP_apocrita.sh
+++ Launch_YAGAP_apocrita.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
-#$ -cwd
-#$ -pe smp 2
-#$ -l h_vmem=4G
-#$ -l h_rt=1:0:0
-#$ -j y
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=2G
+#SBATCH --time=240:00:00
+#SBATCH --output=%x-%j.o
+#SBATCH --error=%x-%j.e
+#SBATCH --job-name=yagap
 
 
 #module load HGI/common/nextflow/24.10.4
 module load nextflow
 
-module load use.own miniconda
+module load use.own miniforge
 #conda activate portcullis-env
-conda activate mikado
+# conda activate mikado
 
 #ml samtools
 
 export NXF_OPTS="-Xms8G -Xmx16G"
 
-work_dir=$1
+genome=$1
+sample_sheet=$2
 
-nextflow run /data/home/btx717/storage/01-ademendoza/software/yagap -w $work_dir -resume -with-trace -with-report --verbose
+nextflow run /data/home/btx717/storage/01-ademendoza/software/yagap -resume -with-trace -with-report --verbose --genome "$genome" --sr_rnaseq_reads "$sample_sheet"
